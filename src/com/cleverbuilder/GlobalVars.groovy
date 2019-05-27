b8168fab0569ccd3e95088cdcec36de8bec0f705
root <vishwajeet.singh@opstree.com>
2019-05-27 20:35:10 +0530

    adding vishwa as variable

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -3,7 +3,7 @@ package com.cleverbuilder
 
 class GlobalVars {
    static String foo = "bar"
-
+   static String vishwa = "vishwajeet singh"
    // refer to this in a pipeline using:
    //
    // import com.cleverbuilder.GlobalVars
